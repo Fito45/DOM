934e827553ac94787630ce8cbd72422948f77ec7
Fito 45 <adolfocastroorozco29@gmail.com>
2025-02-10 17:14:14 -0600

    aprendimos a agregar una nueva tarea con JS

--- 11-ejercicio-tareas/script.js
+++ 11-ejercicio-tareas/script.js
@@ -2,6 +2,7 @@
 
 const taskInput = document.getElementById('taskInput');
 const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
 
 //Escuchador 
 
@@ -12,8 +13,40 @@ addButton.addEventListener('click', createTask);
 function createTask() {
     if (taskInput.value) {
         console.log(taskInput.value);
-        taskInput.value='';
-    } else{
+
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task')
+
+        //creamos el Div de las tareas
+
+        const taskText = document.createElement('p');
+        taskText.innerText = taskInput.value;
+
+        //Creamos el  contenedor de las Iconos
+
+        const taskIcons = document.createElement('div');
+        taskIcons.classList.add('task-icons');
+
+        //creamos el icono Check
+
+        const iconCheck = document.createElement('i');
+        iconCheck.classList.add('bi', 'bi-check', 'icon-complete')
+
+        //Crear icono Trash
+
+        const iconDelete = document.createElement('i');
+        iconDelete.classList.add('bi', 'bi-trash2', 'icon-delete')
+
+        //Estructura de los elementos
+
+        taskIcons.append(iconCheck, iconDelete);
+
+        taskItem.append(taskText, taskIcons);
+
+        taskList.append(taskItem);
+
+        taskInput.value = '';
+    } else {
         alert('eh, escribe una tarea mi judas')
     }
 
